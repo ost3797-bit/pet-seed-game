3db569fd40560efa0ce6e498c6c3fbe74daf6b1c
PetSeedGame Developer <dev@petseedgame.local>
2026-07-29 23:09:26 +0900

    Move interact button 50 pixels to the right

--- scripts/MainField.gd
+++ scripts/MainField.gd
@@ -890,8 +890,8 @@ func _build_ui() -> void:
 	joystick.offset_bottom += 60
 	layer.add_child(joystick)
 	
-	# 상호작용(말하기) 버튼 (20% 축소: 160 -> 130, 전체적으로 60픽셀 하향 조정을 위해 Y=530)
-	_add_touch_button(layer, Vector2(1070, 530), &"interact", "말하기\n(Space)", Vector2(130, 130))
+	# 상호작용(말하기) 버튼 (20% 축소: 160 -> 130, X축 우측으로 50픽셀 이동)
+	_add_touch_button(layer, Vector2(1120, 530), &"interact", "말하기\n(Space)", Vector2(130, 130))
 
 	_build_dialogue_panel(layer)
 
